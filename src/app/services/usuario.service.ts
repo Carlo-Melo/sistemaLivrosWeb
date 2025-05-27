666fc0be37fc095443a447d1e483b6d640dfe9e1
Carlo-melo <ca46220@gmail.com>
2025-05-27 20:58:33 -0300

    first commit

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,32 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Livro } from '../models/livro';
+
+@Injectable({ providedIn: 'root' })
+export class LivroService {
+  private apiUrl = 'http://localhost:8080/api/livros';
+
+  constructor(private http: HttpClient) {}
+
+  listarTodos(): Observable<Livro[]> {
+    return this.http.get<Livro[]>(this.apiUrl);
+  }
+
+  buscarPorId(id: number): Observable<Livro> {
+    return this.http.get<Livro>(`${this.apiUrl}/${id}`);
+  }
+
+  cadastrar(livro: Livro): Observable<Livro> {
+    return this.http.post<Livro>(this.apiUrl, livro);
+  }
+
+  atualizar(id: number, livro: Livro): Observable<Livro> {
+    return this.http.put<Livro>(`${this.apiUrl}/${id}`, livro);
+  }
+
+  deletar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
